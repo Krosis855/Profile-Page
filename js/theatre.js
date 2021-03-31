7d80ba69e81586f7c6527247c9e1288a40d762dc
Krosis855 <jtspalding22@gmail.com>
2021-03-31 15:12:50 -0700

    sourpuss

---
+++ js/theatre.js
@@ -0,0 +1,10 @@
+const GENERAL_ADMISSION_TICKET_COST = 20;
+const CHILD_AND_SENIOR_TICKET_COST = 10;
+
+function buyTicket() {
+    const age = prompt('What is your age?');
+    let cost = GENERAL_ADMISSION_TICKET_COST;
+    if ((age <= 12) || (age >= 65)) {
+        alert(`Your ticket is $${cost}.`);
+    }
+}
